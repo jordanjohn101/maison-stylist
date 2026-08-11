71915a6b5417670dc739464d5a5de9413c092e3a
jordanjohn101 <jordanjohntownsend101@gmail.com>
2026-08-11 19:22:45 +1000

    updated SW

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "maison-v2";
+const ASSETS = [
+  "/maison-stylist/",
+  "/maison-stylist/index.html",
+  "/maison-stylist/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      })
+    ).catch(() => caches.match("/maison-stylist/index.html"))
+  );
+});
